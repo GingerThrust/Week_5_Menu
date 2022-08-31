db13555918bf53e7cfce0c55394c11e61e6b309b
GingerThrust <mbrown9388@gmail.com>
2022-08-31 15:30:19 -0500

    Declaration of what the app will do

---
+++ Game_Menu_App.js
@@ -0,0 +1,4 @@
+/*This app is going to be a menu app to help catalog the games I have completed in 2022. I plan on allowing
+it to add individual titles to the list. Also, I want to catagorize them by platform, like PlayStation,
+Xbox, PC, etc.  
+*/
