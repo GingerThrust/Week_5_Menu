37ccff2b9a3d3d0dd0221d480ed7a99f04838df9
GingerThrust <mbrown9388@gmail.com>
2022-09-02 03:44:41 -0500

    Final homework assignment

--- Game_Menu_App.js
+++ Game_Menu_App.js
@@ -3,19 +3,19 @@ it to add individual titles to the list. Also, I want to catagorize them by plat
 Xbox, PC, etc.  
 */
 class Game {
-    constructor (name, month, review) {
+    constructor(name, month, review) {
         this.name = name;
         this.month = month;
         this.review = review;
     }
 
-    opinion () {
-        return `I played ${this.name} on ${this.date} and I thought it was ${this.review}.`;
+    opinion() {
+        return `I played ${this.name} on ${this.month} and I thought it was ${this.review}.`;
     }
 }
 
 class Platform {
-    constructor (name) {
+    constructor(name) {
         this.name = name;
         this.games = [];
     }
@@ -29,18 +29,18 @@ class Platform {
     }
 
     listing () {
-        return `This year on the ${this.name} I played ${this.games}. `;
+        return `This year on the ${this.name} I played ${this.games}.`;
     }
 }
 
 
 class Menu {
-    constructor () {
+    constructor() {
         this.platforms = [];
         this.selectedPlatform = null;
     }
 
-    start () {
+    start() {
         let choice = this.MainMenuOptions();
         while (choice != 0) {
             switch (choice) {
@@ -74,6 +74,25 @@ class Menu {
         `)
     }
 
+    showPlatformMenuOptions(platformInfo) {
+        return prompt(`
+        (0) Back
+        (1) Add game to platform
+        (2) Remove game from platform
+        -----------------------------
+
+        ${platformInfo}
+        `);
+    }
+
+    seeAllPlatforms() {
+        let platformString = '';
+        for (let i = 0; i < this.platforms.length; i++) {
+            platformString += i + ") " + this.platforms[i].name + "\n";
+        } 
+        alert(platformString);
+    }
+
     logNewPlatform() {
         let name = prompt("Enter the name of the new platform to add: ");
         this.platforms.push(new Platform(name));
@@ -81,39 +100,26 @@ class Menu {
     }
 
     viewPlatform() {
-        let index = prompt("Enter the name of the platform you wish to see: ");
+        let index = prompt("Enter the index of the platform you wish to see: ");
         if (index > -1 && index < this.platforms.length) {
             this.selectedPlatform = this.platforms[index];
-            let fullSelection = "Platform name " + this.selectedPlatform.name + "\n";
-            for (i = 0; i < this.selectedPlatform.games.length; i++) {
-            fullSelection += i + ") " + this.selectedPlatform.games[i].name + "\n";
+            let description = "Platform name " + this.selectedPlatform.name + "\n";
+           
+           
+            for (let i = 0; i < this.selectedPlatform.games.length; i++) {
+            description += i + ") " + this.selectedPlatform.games[i].name + ' - ' + 
+            this.selectedPlatform.games[i].month + ' - ' + this.selectedPlatform.games[i].review + "\n";
             }
 
-            let selection = this.showPlatformMenuOptions(fullSelection)
+            let selection = this.showPlatformMenuOptions(description);
             switch (selection) {
                 case "1":
-                    this.addGame();
+                    this.createGame();
                     break;
                 case "2":
                     this.deleteGame();
             }
         }
-    }
-
-    showPlatformMenuOptions(platformInfo) {
-        return prompt(`
-        (0) Back
-        (1) Add game to platform
-        (2) Remove game from platform
-        `);
-    }
-    
-    seeAllPlatforms() {
-        let platformString = '';
-        for (let i = 0; i < this.platforms.length; i++) {
-            platformString += i + ") " + this.platforms[i].name + "\n";
-        } 
-        alert(platformString);
     }
 
     deletePlatform() {
@@ -123,15 +129,17 @@ class Menu {
         }
      }
 
-    addGame() {
+    createGame() {
         let name = prompt("Enter the name of the game you wish to add: ");
-        this.selectedPlatform.games.push(new Game(game));
+        let month = prompt("Enter the month you played this game: ");
+        let review = prompt("Enter your opinion of the game by the following words: Bad, Ok, Good, Great, Amazing:");
+        this.selectedPlatform.games.push(new Game(name, month, review));
     }
 
     deleteGame() {
-        let name = prompt("Enter the index of the game you wish to delete: ");
-        if (name > -1 && name < this.selectedPlatforms.games.length) {
-        this.selectedPlatform.games.splice(index,1);
+        let index = prompt("Enter the index of the game you wish to delete: ");
+        if (index > -1 && index < this.selectedPlatform.games.length) {
+        this.selectedPlatform.games.splice(index, 1);
         }
     }
  }
@@ -140,3 +148,4 @@ class Menu {
 
 let menu = new Menu();
 menu.start();
+
